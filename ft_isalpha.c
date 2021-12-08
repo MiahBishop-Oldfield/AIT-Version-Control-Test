515c5d9f48dcabe90f32a467ce970fe92fca582f
lmells98 <86273901+lmells98@users.noreply.github.com>
2021-12-08 15:09:37 +1030

    Changed is apha to check for not alpha
    Output will still remain true as before

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -3,10 +3,10 @@
 int	ft_isalpha(int c)
 
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
+	if (!(c >= 'a' && c <= 'z') || !(c >= 'A' && c <= 'Z'))
 		return (0);
+	else
+		return (1);
 }
 
 /* Tests for alphabetic characters. */
